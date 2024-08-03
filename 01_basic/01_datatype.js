258d41b3ca880e1700bbc218e564b2b618b34987
Pooja-Vr <150262023+Pooja-Vr@users.noreply.github.com>
2024-08-03 12:25:41 +0000

    datatypes

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+5) we are using node.js, not browser
+
+console.log(3
+   +6);    //code readablity should be high
+
+console.log("pooja");
+
+let name= "pooja"
+let age= 18
+let loggin=false
+
+// number => 2 to power 53
+
+//bigInt
+//string => ""
+//boolean => true/false
+//Null => standalone value
+//undefine => 
+//symbol => unique
+
+//Object
+console.log(typeof age);
+console.table([typeof age, loggin, name]);
+console.log(typeof loggin)
+
+console.log(typeof undefined)//undefine
+console.log(typeof null)//object
