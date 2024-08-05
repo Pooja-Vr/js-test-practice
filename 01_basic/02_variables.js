d2f363d3457cf4018ac78e0c05a5f869d1d6eb84
Pooja-Vr <150262023+Pooja-Vr@users.noreply.github.com>
2024-08-05 07:05:25 +0000

    CoversionOperation

---
+++ 01_basic/02_variables.js
@@ -0,0 +1,51 @@
+// const accountId = 4435261
+// let accountEmail="pooja123@gmail.com"
+// var accountPassword ="235422"
+// accountCity="Jaipur"
+// let accounState;
+
+// // accountId=2  not allowed 
+// accountEmail="34@gmail.com"
+// accountPassword="453467"
+// accountCity="Delhi"
+
+// // {} = called scope
+
+// console.log(accountId)
+// console.table([accountId, accountEmail, accountPassword, accountCity, accounState]);
+
+// function program(){
+//     let a="5"; //5 is string now
+//     let b=3;   // and 3 is int now
+//     let c= a*b;// but when we get multiple a & b that 5 we'll be int now =15ans
+//     return c-b;
+// }
+
+// console.log(program());
+
+// function program(){
+//     let a="5";
+//     let b=3;
+//     let c= a+b;
+//     return c-b;
+// }
+
+// console.log(program());
+
+// function program(){
+//     let a="5";
+//     let b=3;
+//     let c= a-b;
+//     return c-b;
+// }
+
+// console.log(program());
+
+function program(){
+    let a="5";
+    let b=3;
+    let c= a/b;
+    return c-b;
+}
+
+console.log(program());
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score=56;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let score1="56";
+console.log(typeof score1);
+console.log(typeof (score1));
+
+let valueInNumber = Number(score1)
+console.log(Typeof, valueInNumber);
