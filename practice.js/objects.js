efbb2ab53f8a3aa99b2f4e97731830d8be5973c9
Pooja-Vr <150262023+Pooja-Vr@users.noreply.github.com>
2024-08-07 13:20:05 +0000

    arrays and loops

--- practice.js/objects.js
+++ practice.js/objects.js
@@ -19,12 +19,12 @@
 // objectName.propertyName
 // objectName["propertyName"]
 
-const person = {
-    firstName: "Pooja",
-    lastName : "Verma",
-    id       : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-    console.log(firstName)
-  };
+// const person = {
+//     firstName: "Pooja",
+//     lastName : "Verma",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//     console.log(firstName)
+//   };
